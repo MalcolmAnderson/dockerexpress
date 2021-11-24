3d6085634b88a0c8148473c4ca6e0fcca675ef98
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2021-11-23 22:13:00 -0800

    2:23:41 - about to start basic crud app

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/c
 
 const app = express();
 
-// mongoose.connect("mongodb://mba:mba@192.168.192.2:27017/?authSource=admin")
-mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
-.then(() => console.log("successfully connected to DB"))
-.catch((e) => console.log(e));
+// current retry logic is better than nothing
+//      ideally this would set some kind of flag so that an error page 
+//          would be loaded instead of the normal app page.
+//      Even better would be to have internal and external users, 
+//          so that internal users could see the error and external 
+//          users would see what the business considered "appropriate".
+const connectWithRetry = () => {
+    mongoose
+    .connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
+    .then(() => console.log("successfully connected to DB"))
+    .catch((e) => {
+        console.log(e)
+        setTimeout(connectWithRetry, 5000)
+    });
+};
+// mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
+// .then(() => console.log("successfully connected to DB"))
+// .catch((e) => console.log(e));
+connectWithRetry();
 
 app.get("/", (req, res) => {
-    res.send("<h2>Hi There - changing stuff</h2>");
+    res.send("<h2>Hi There - now with retry logic</h2>");
 });
 
 const port = process.env.PORT || 3000;
